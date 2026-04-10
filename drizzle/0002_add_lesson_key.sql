4efc4a0dadcb6c1b2785240393419753fb27428d
johnfinleyproductions-lang <johnfinleyproductions@gmail.com>
2026-04-09 22:15:37 -0500

    Remove vault migration: 0002_add_lesson_key

--- drizzle/0002_add_lesson_key.sql
+++
@@ -1,3 +0,0 @@
--- Add lesson_key column for grouping workflows with their documentation
-ALTER TABLE automations ADD COLUMN lesson_key TEXT;
-CREATE INDEX idx_automations_lesson_key ON automations(lesson_key);
